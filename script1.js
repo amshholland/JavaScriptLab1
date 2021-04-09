5c1f703d5286a3203cfb1c98e5da5cc722af3254
Amber Holland <Amber.S.Creech@gmail.com>
2021-04-09 14:39:30 -0400

    fixed overtime calculation

--- script1.js
+++ script1.js
@@ -7,9 +7,9 @@ if (hours <= 40) {
     console.log(`$${total.toFixed(2)}`);
 }
 else {
+    total = ((hours - 40) * (wage * 1.5)) + (40 * wage);
     console.log(`$${total.toFixed(2)}`);
 }
 
 let millionthBuck = Math.round(1000000 / total);
 console.log(`It will take you about ${millionthBuck} weeks to make $1,000,000.00.`);
-
