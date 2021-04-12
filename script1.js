829932c5b1eadc46e219bf1f6bef818189fd689a
Amber Holland <Amber.S.Creech@gmail.com>
2021-04-12 08:11:49 -0400

    complete

--- script1.js
+++ script1.js
@@ -4,12 +4,12 @@ let wage = prompt('What is your hourly pay?');
 let total = hours * wage;
 
 if (hours <= 40) {
-    console.log(`$${total.toFixed(2)}`);
+    console.log(`You made $${total.toFixed(2)} this week`);
 }
 else {
     total = ((hours - 40) * (wage * 1.5)) + (40 * wage);
-    console.log(`$${total.toFixed(2)}`);
+    console.log(`You made $${total.toFixed(2)} this week`);
 }
 
 let millionthBuck = Math.round(1000000 / total);
-console.log(`It will take you about ${millionthBuck} weeks to make $1,000,000.00.`);
+console.log(`If this is how much you make every week, it will take you about ${millionthBuck} weeks to make $1,000,000.00.`);
